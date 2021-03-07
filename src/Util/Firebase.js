faf229e45ae9cc302c65d17f7471a101cbb7a950
anaelshomrai <anaelshomrai@gmail.com>
2021-03-07 14:24:26 +0200

    Firebase config

--- src/Util/Firebase.js
+++ src/Util/Firebase.js
@@ -5,18 +5,25 @@ import 'firebase/storage';
 import moment from "moment";
 import 'firebase/analytics';
 import 'firebase/performance';
+const { REACT_APP_FIREBASE_APP_KEY,REACT_APP_FIREBASE_AUTH_DOMAIN,REACT_APP_FIREBASE_PROJECT_ID
+,REACT_APP_FIREBASE_STORAGE_BUCKET,REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+, REACT_APP_FIREBASE_APP_ID,REACT_APP_FIREBASE_MEASUREMENT_ID} = process.env;
+
+
  //to export to env file
 let firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APP_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    apiKey: REACT_APP_FIREBASE_APP_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
     //databaseURL: "https://test-f8ea4.firebaseio.com",
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
+    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
     //sotrageURL: "gs://test-f8ea4.appspot.com/",
   };
+
+  console.log(firebaseConfig);
 
 // const config = {
 //   apiKey: process.env.REACT_APP_API_KEY,
